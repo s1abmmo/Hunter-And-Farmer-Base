0464b252ed6c5650d135553e9a6a1c6e5a4107b6
Hai Dang <s1abmmo@gmail.com>
2024-05-09 07:09:30 +0700

    change unity version 2022.3.27f + add zoom camera

---
+++ Assets/Scripts/CameraZoomScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomScript : MonoBehaviour
+{
+    public float perspectiveZoomSpeed = 0.1f;        // Tốc độ zoom cho camera Perspective
+    public float orthoZoomSpeed = 0.1f;
+
+    public float MinZoomPerspectiveCamera = 15f;
+    public float MaxZoomPerspectiveCamera = 80f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Chỉ sử dụng khi có đúng hai ngón tay chạm vào màn hình.
+        if (Input.touchCount == 2)
+        {
+            // Lưu trữ hai ngón tay chạm vào.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Tìm vị trí của từng ngón tay trong frame trước.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Tính toán khoảng cách giữa các vị trí của từng ngón tay trong frame hiện tại và frame trước.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Tính toán thay đổi khoảng cách giữa các frames.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // Nếu camera sử dụng perspective
+            if (GetComponent<Camera>().orthographic)
+            {
+                // Thay đổi kích thước của camera theo orthographic size.
+                GetComponent<Camera>().orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+                // Đảm bảo orthographic size không âm.
+                GetComponent<Camera>().orthographicSize = Mathf.Max(GetComponent<Camera>().orthographicSize, 0.1f);
+            }
+            else
+            {
+                // Thay đổi Field of View của camera dựa trên thay đổi khoảng cách.
+                GetComponent<Camera>().fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+                // Đảm bảo field of view trong giới hạn hợp lý.
+                GetComponent<Camera>().fieldOfView = Mathf.Clamp(GetComponent<Camera>().fieldOfView, MinZoomPerspectiveCamera, MaxZoomPerspectiveCamera);
+            }
+        }
+    }
+}
--- Assets/Scripts/FarmerCharacterScript.cs
+++ Assets/Scripts/FarmerCharacterScript.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Tree;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class FarmerCharacterScript : MonoBehaviour
